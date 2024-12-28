e6fc5e83684865946da7a3daddad88eb7b0cf7fe
Parker <parkersam@gmail.com>
2024-12-27 23:21:53 -0700

    Added color and filter classes

---
+++ wordlebuddy-shared/src/model/Color.ts
@@ -0,0 +1,39 @@
+interface Color {
+  hex: string;
+  next: string;
+  filterMethod: (word: string, char: string, pos: number) => boolean;
+}
+
+export class Colors {
+  public static readonly black = "b";
+  public static readonly yellow = "y";
+  public static readonly green = "g";
+  public static readonly orange = "o";
+
+  public static get(colorCode: string): Color {
+    return this.dataOf[colorCode];
+  }
+
+  private static readonly dataOf: { [key: string]: Color } = {
+    [this.black]: {
+      hex: "#3a3a3c",
+      next: this.yellow,
+      filterMethod: (word: string, char: string, pos: number) => !word.includes(char),
+    },
+    [this.yellow]: {
+      hex: "#b59f3b",
+      next: this.green,
+      filterMethod: (word: string, char: string, pos: number) => word.includes(char) && word[pos] !== char,
+    },
+    [this.green]: {
+      hex: "#538d4e",
+      next: this.black,
+      filterMethod: (word: string, char: string, pos: number) => word[pos] === char,
+    },
+    [this.orange]: {
+      hex: "#ff8c00",
+      next: this.orange,
+      filterMethod: (word: string, char: string, pos: number) => word.includes(char),
+    },
+  };
+}
---
+++ wordlebuddy-shared/src/model/Filter.ts
@@ -0,0 +1,38 @@
+import { Colors } from "./Color";
+import { Guess } from "./Guess";
+
+export class Filter {
+  private _character: string;
+  private _position: number;
+  private _color: string;
+
+  constructor(character: string, position: number, color: string) {
+    this._character = character;
+    this._position = position;
+    this._color = color;
+  }
+
+  get character(): string {
+    return this._character;
+  }
+
+  get color(): string {
+    return this._color;
+  }
+
+  get position(): number {
+    return this._position;
+  }
+
+  public filter(word: string): boolean {
+    return Colors.get(this._color).filterMethod(word, this._character, this._position);
+  }
+
+  public static fromGuess(guess: Guess): Filter[] {
+    const filters: Filter[] = [];
+    for (let i = 0; i < guess.word.length; i++) {
+      filters.push(new Filter(guess.word[i], i, guess.colors[i]));
+    }
+    return filters;
+  }
+}

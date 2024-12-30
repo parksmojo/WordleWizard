8a416adaa867e0f8407566979ef55779d12390e9
Parker <parkersam@gmail.com>
2024-12-29 20:00:58 -0700

    Changed tab title

--- wordlewizard-web/src/app/app.routes.ts
+++ wordlewizard-web/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, title: 'Wordle Wizard' },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: HomeComponent },
 ];

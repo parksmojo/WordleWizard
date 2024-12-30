64ef060ef6afc6c1379fc68c90046333d9950c04
Parker <parkersam@gmail.com>
2024-12-30 00:35:48 -0700

    Add Firebase configuration and hosting setup

--- wordlewizard-web/src/main.ts
+++ wordlewizard-web/src/main.ts
@@ -2,5 +2,28 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDpWDz-i8vGRVVJkiurDqinj2WraZrwgOU',
+  authDomain: 'wordle-wizard.firebaseapp.com',
+  projectId: 'wordle-wizard',
+  storageBucket: 'wordle-wizard.firebasestorage.app',
+  messagingSenderId: '1037865245539',
+  appId: '1:1037865245539:web:7aaf2cbd2696d7b5be1156',
+  measurementId: 'G-QVQ3VQRBWN',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);

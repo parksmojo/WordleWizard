e566d0da38f82e7ec02e12e799ba4103d8ed9f15
Parker <parkersam@gmail.com>
2025-01-01 23:25:56 -0700

    Refactor header component layout

--- wordlewizard-web/src/app/pages/about/about.component.ts
+++ wordlewizard-web/src/app/pages/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
-
+  constructor(protected route: Router) {}
 }

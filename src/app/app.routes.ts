547df96380a8f07881014bcfa852b4770b45c2b1
Parker <parkersam@gmail.com>
2025-01-24 18:09:30 -0700

    Minor changes and cleanup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,11 @@ import { AboutComponent } from '../pages/about/about.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent, title: 'Wordle Wizard' },
-  { path: 'about', component: AboutComponent, title: 'Wordle Wizard' },
-  { path: '', redirectTo: '/about', pathMatch: 'full' },
-  { path: '**', component: AboutComponent },
+  {
+    path: '',
+    pathMatch: 'full',
+    component: AboutComponent,
+    title: 'Wordle Wizard',
+  },
+  { path: '**', redirectTo: '' },
 ];

936c1a0bd36fa9dbcd6ed7592be3e233335dfec0
Parker <parkersam@gmail.com>
2025-01-25 19:15:25 -0700

    Updated filter tests

--- src/model/word-list/word-list.spec.ts
+++ src/model/word-list/word-list.spec.ts
@@ -1,7 +1,21 @@
+import { Guess } from '../guess/guess';
 import { WordList } from './word-list';
 
 describe('WordList', () => {
+  let list: WordList;
+
+  beforeEach(() => {
+    list = new WordList();
+  });
+
   it('should create an instance', () => {
-    expect(new WordList()).toBeTruthy();
+    expect(list).toBeTruthy();
+  });
+
+  it('should filter new yellows correctly', () => {
+    list.filterByGuess(new Guess('spark', 'yybyb'));
+    list.filterByGuess(new Guess('ports', 'ybyby'));
+    expect(list.possibleGuesses).toContain('crisp');
+    expect(list.possibleGuesses.length).toBe(1);
   });
 });

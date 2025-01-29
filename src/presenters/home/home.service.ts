db747007721848dd49a0e8be2767acee3ea35f51
Parker <parkersam@gmail.com>
2025-01-28 23:43:08 -0700

    Add user authentication check and stats retrieval to HomeService

--- src/presenters/home/home.service.ts
+++ src/presenters/home/home.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { FirestoreService } from '../../services/firestore/firestore.service';
 import { Guess } from '../../model/guess/guess';
+import { UserStats } from '../../model/user-stats/user-stats';
 
 @Injectable({
   providedIn: 'root',
@@ -10,12 +11,23 @@ export class HomeService {
   private auth = inject(AuthService);
   private db = inject(FirestoreService);
 
+  public isSignedIn() {
+    return this.auth.isSignedIn();
+  }
+
   public async recordGuess(guess: Guess): Promise<void> {
     const user = this.auth.getCurrentUser();
     if (!user) {
       return;
     }
-
     await this.db.saveGuess(user, guess);
+  }
+
+  public async getStats(): Promise<UserStats> {
+    const user = this.auth.getCurrentUser();
+    if (!user) {
+      throw new Error('Logged out user tried to get stats');
+    }
+    return await this.db.getUserStats(user);
   }
 }
